c1bfa3915d51d6c17eabff1760a66197707b1905
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-09 21:35:00 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/Interpreters.pm
@@ -0,0 +1,24 @@
+package Bencher::Scenario::Interpreters;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    participants => [
+        {type=>'command', name=>'perl'  , cmdline=>[qw/perl -e1/]},
+        {type=>'command', name=>'bash'  , cmdline=>[qw/bash --norc -c true/]},
+        {type=>'command', name=>'ruby'  , cmdline=>[qw/ruby -e1/]},
+        {type=>'command', name=>'python', cmdline=>[qw/python -c1/]},
+        {type=>'command', name=>'nodejs', cmdline=>[qw/nodejs -e 1/]},
+    ],
+};
+
+# ABSTRACT: Benchmark startup time of various interpreters
+
+=head1 SYNOPSIS
+
+ % bencher -m Interpreters [other options]...
