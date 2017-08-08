0b6802eab7c651b55d92c1d8e8e7e9173588f095
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-08-08 19:29:55 +0700

    Add participants: perl -E, python -S, python -S+exit

--- lib/Bencher/Scenario/Interpreters.pm
+++ lib/Bencher/Scenario/Interpreters.pm
@@ -10,11 +10,14 @@ use warnings;
 our $scenario = {
     summary => 'Benchmark startup time of various interpreters',
     participants => [
-        {name=>'perl'  , cmdline=>[qw/perl -e1/]},
-        {name=>'bash'  , cmdline=>[qw/bash --norc -c true/]},
-        {name=>'ruby'  , cmdline=>[qw/ruby -e1/]},
-        {name=>'python', cmdline=>[qw/python -c1/]},
-        {name=>'nodejs', cmdline=>[qw/nodejs -e 1/]},
+        {name=>'perl (-e)'  , cmdline=>[qw/perl -e1/]},
+        {name=>'perl (-E)'  , cmdline=>[qw/perl -E1/]},
+        {name=>'bash'       , cmdline=>[qw/bash --norc -c true/]},
+        {name=>'ruby'       , cmdline=>[qw/ruby -e1/]},
+        {name=>'python'        , cmdline=>[qw/python -c1/]},
+        {name=>'python -S'     , cmdline=>[qw/python -S -c1/]},
+        {name=>'python -S+exit', cmdline=>['python', '-S', '-c', 'from os import _exit; _exit(0)']},
+        {name=>'nodejs'     , cmdline=>[qw/nodejs -e 1/]},
     ],
     on_failure => 'skip',
 };
